28a3c5fe880c860ffd9c06c1b3b6510d1bff359a
Gopalbhadouriya <gopalbhadouriya32@gmail.com>
2024-07-28 00:59:18 -0700

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,16 @@
+greating = 'RAM-RAM'
+print(greating)
+sub = 'I am going to tell you a poem'
+print(sub)
+
+
+print("twinkle-twinkle little star,")
+
+
+print("how are wonder what you are,")
+print("once upon the up to high,")
+print("like a daimond in the sky.")
+
+#this is a commit which can not be show in console it show only in code
+
+
